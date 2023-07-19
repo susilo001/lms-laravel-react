95ef1927395e4b74cc882fc91cef419e0bda63f9
susilo001 <susilo.2000@students.amikom.ac.id>
2023-07-19 22:08:44 +0700

    Add Spatie Roles and Permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,16 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\GradeSeeder;
+use Database\Seeders\LessonSeeder;
+use Database\Seeders\StudentSeeder;
+use Database\Seeders\TeacherSeeder;
+use Database\Seeders\ClassRoomSeeder;
+use Database\Seeders\AssignmentSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +22,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RolesAndPermissionSeeder::class,
             UserSeeder::class,
+            TeacherSeeder::class,
+            StudentSeeder::class,
+            LessonSeeder::class,
+            GradeSeeder::class,
+            ClassRoomSeeder::class,
+            AssignmentSeeder::class,
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
@@ -17,6 +18,17 @@ class UserSeeder extends Seeder
             'name' => 'Bio',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('password'),
-        ]);
+        ])->assignRole('admin');
+
+        $students = User::factory()->count(10)->create();
+        $teachers = User::factory()->count(10)->create();
+
+        $students->each(function ($student) {
+            $student->assignRole('student');
+        });
+
+        $teachers->each(function ($teacher) {
+            $teacher->assignRole('teacher');
+        });
     }
 }
